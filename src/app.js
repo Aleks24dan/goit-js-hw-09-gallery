b76290fc06f0cc89e46dfae63c8606620a1eecac
Aleks24dan <aleks24dan@gmail.com>
2021-08-14 16:23:27 +0200

    ddd

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-export default[
+import f from './images/himilayan-blue-poppy-4202825__340.jpg';
+export default [
 
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+      f,
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
     description: 'Hokkaido Flower',
